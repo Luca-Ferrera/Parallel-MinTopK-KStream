f3f6fbef9b38cf06d0bcf29547eb63a511b12851
Luca-Ferrera <luca.ferrera96@gmail.com>
2020-04-18 16:49:22 +0200

    Forwarding only updates in local topK

--- src/main/java/myapp/punctuators/TopKPunctuator.java
+++ src/main/java/myapp/punctuators/TopKPunctuator.java
@@ -16,20 +16,30 @@ import static java.lang.Math.min;
 
 public class TopKPunctuator implements Punctuator {
 
-    private KeyValueStore<String, ValueAndTimestamp<ScoredMovie>> state;
+    private KeyValueStore<String, ValueAndTimestamp<ScoredMovie>> state1;
+    private KeyValueStore<String, ScoredMovie> state2;
     private ProcessorContext context;
     private int k;
+    private boolean isState2;
 
-    public TopKPunctuator(KeyValueStore state, ProcessorContext context, int k) {
-        this.state = state;
+    public TopKPunctuator(KeyValueStore state1, ProcessorContext context, int k) {
+        this.state1 = state1;
         this.context = context;
         this.k = k;
+        this.isState2 = false;
+    }
+    public TopKPunctuator(KeyValueStore state1, KeyValueStore state2, ProcessorContext context, int k) {
+        this.state1 = state1;
+        this.state2 = state2;
+        this.context = context;
+        this.k = k;
+        this.isState2 = true;
     }
 
     @Override
     public void punctuate(long l) {
         ArrayList<KeyValue<String, ScoredMovie>> scoreList = new ArrayList<>();
-        this.state.all().forEachRemaining((elem)-> {
+        this.state1.all().forEachRemaining((elem)-> {
             KeyValue<String,ScoredMovie> entry = new KeyValue<>(elem.key, elem.value.value());
             scoreList.add(entry);
         });
@@ -37,6 +47,31 @@ public class TopKPunctuator implements Punctuator {
         Collections.sort(scoreList, compareByScore.reversed()
         );
         List<KeyValue<String, ScoredMovie>> topKList = scoreList.subList(0, min(this.k, scoreList.size()));
-        topKList.forEach(elem -> context.forward(elem.key, elem.value));
+        //enter here if intermediate topKPhase
+        if(this.isState2) {
+            if(!this.state2.all().hasNext()) {
+                final Integer[] i = {1};
+                topKList.forEach(elem -> {
+                    this.state2.put(i[0].toString(), elem.value);
+                    context.forward(elem.key, elem.value);
+                    i[0]++;
+                });
+            } else {
+                final Integer[] position = {1};
+                topKList.forEach(elem -> {
+                    this.state2.all().forEachRemaining(stateElem -> {
+                        // forward only if there are updates in the topKList
+                        if (stateElem.key.equals(position[0].toString()) && stateElem.value.getId() != elem.value.getId()) {
+                            this.state2.put(position[0].toString(), elem.value);
+                            context.forward(elem.key, elem.value);
+                        }
+                    });
+                    position[0]+=1;
+                });
+            }
+        } else {
+            // used by the centralized aggregator for final topK results
+            topKList.forEach(elem -> context.forward(elem.key, elem.value));
+        }
     }
 }
--- src/main/java/myapp/transormers/TopKTransformer.java
+++ src/main/java/myapp/transormers/TopKTransformer.java
@@ -11,23 +11,38 @@ import org.apache.kafka.streams.state.ValueAndTimestamp;
 import java.time.Duration;
 
 public class TopKTransformer implements Transformer<String, ScoredMovie, ScoredMovie> {
-    private KeyValueStore<String, ValueAndTimestamp<ScoredMovie>> state;
+    private KeyValueStore<String, ValueAndTimestamp<ScoredMovie>> state1;
+    private KeyValueStore<String, ValueAndTimestamp<ScoredMovie>> state2;
     private int k;
-    private String storeName;
-    public TopKTransformer(int k, String storeName) {
+    private String storeName1;
+    private String storeName2;
+
+    public TopKTransformer(int k, String storeName1) {
         this.k = k;
-        this.storeName = storeName;
+        this.storeName1 = storeName1;
+        this.storeName2 = "";
+    }
+
+    public TopKTransformer(int k, String storeName1, String storeName2) {
+        this.k = k;
+        this.storeName1 = storeName1;
+        this.storeName2 = storeName2;
     }
 
     public void init(ProcessorContext context) {
-        this.state = (KeyValueStore<String, ValueAndTimestamp<ScoredMovie>>) context.getStateStore(this.storeName);
-        // punctuate each 10 second, can access this.state
-        context.schedule(Duration.ofSeconds(10), PunctuationType.WALL_CLOCK_TIME, new TopKPunctuator(this.state, context, k));
+        this.state1 = (KeyValueStore<String, ValueAndTimestamp<ScoredMovie>>) context.getStateStore(this.storeName1);
+        if(!this.storeName2.isEmpty()) {
+            this.state2 = (KeyValueStore<String, ValueAndTimestamp<ScoredMovie>>) context.getStateStore(this.storeName2);
+            context.schedule(Duration.ofSeconds(10), PunctuationType.WALL_CLOCK_TIME, new TopKPunctuator(this.state1, this.state2, context, k));
+        } else {
+            // punctuate each 10 second, can access this.state
+            context.schedule(Duration.ofSeconds(10), PunctuationType.WALL_CLOCK_TIME, new TopKPunctuator(this.state1, context, k));
+        }
     }
 
     public ScoredMovie transform(String key, ScoredMovie value) {
         // can access this.state
-        return value;
+        return null;
     }
 
     public void close() {
